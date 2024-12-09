46ca22b573e990e5e0def95712723d3adaff8f98
KAkos27 <akoskosztolanyi@gmail.com>
2024-12-09 09:20:33 +0100

    f1 3

--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -5,4 +5,14 @@ public class Tabla {
     private char[][] T;
     private char uresCella;
 
+    public Tabla(char uresCella) {
+        T = new char[8][8];
+        this.uresCella = uresCella;
+
+        for (int sor = 0; sor < T.length ; sor++) {
+            for (int oszlop = 0; oszlop < T.length; oszlop++) {
+                T[sor][oszlop] = this.uresCella;
+            }
+        }
+    }
 }
