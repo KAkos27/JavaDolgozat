ca4f80b3162ebc1766a97c1c2a2987b667ef4b91
KAkos27 <akoskosztolanyi@gmail.com>
2024-12-09 10:59:25 +0100

    f3 1

---
+++ src/main/java/hu/szamalk/modell/Projektor.java
@@ -0,0 +1,29 @@
+package hu.szamalk.modell;
+
+
+import java.util.Random;
+
+public class Projektor {
+
+    private static char[] nev = new char[]{'C', 's', 'o', 'l', 't', 'i', 'P', 'e', 't', 'i'};
+    private static char[] abc = new char[]{'a', 'A', 'b', 'B', 'c', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L'};
+    private static Random rnd = new Random();
+
+    private char[] veletlenBetuk;
+
+    public Projektor() {
+        this.veletlenBetuk = veletlenBetukFeltolt();
+    }
+
+    private char[] veletlenBetukFeltolt() {
+        char[] veletlen = new char[10];
+
+        for (int i = 0; i < 10; i++) {
+            int szam = rnd.nextInt(abc.length);
+            veletlen[i] = abc[szam];
+        }
+
+        return veletlen;
+    }
+
+}
